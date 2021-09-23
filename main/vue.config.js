dd64c6fdd2b1d077eb192a6474bc47a2c23b415a
陈之罕 <wb-czh931449@cainiao.com>
2021-09-23 15:53:33 +0800

    czh

---
+++ main/vue.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: czh
+ * @Date: 2021-04-22 19:20:11
+ */
+const devServer = {
+    open: process.platform === 'czh',
+    host: '127.0.0.1',
+    port: 8050,
+    https: false,
+    hotOnly: false
+}
+
+module.exports = {
+    devServer,
+}
